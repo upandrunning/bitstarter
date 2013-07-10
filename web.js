2826baf2f73f0347bb6ff1d88f00cf021673a033
Ubuntu <ubuntu@ip-172-31-24-168.us-west-2.compute.internal>
2013-07-10 17:21:22 +0000

    file sync stuff

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
+
+var filereadbits = fs.readFileSync('index.html');
+
+var fileBuffer = new Buffer(filereadbits);
+var fileString = fileBuffer.toString();
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Worldb!');
+  
+
+	response.send(fileString);
 });
 
 var port = process.env.PORT || 5000;
