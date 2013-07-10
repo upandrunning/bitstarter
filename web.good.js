90ea84208bd13214c0079a281406207245927af5
richard mross <info@uar.biz>
2013-07-10 18:05:46 +0000

    added good

---
+++ web.good.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var fs = require('fs');
+
+var filereadbits = fs.readFileSync('index.html');
+
+var fileBuffer = new Buffer(filereadbits);
+
+var fileString = fileBuffer.toString();
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  
+
+	response.send(fileString);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
